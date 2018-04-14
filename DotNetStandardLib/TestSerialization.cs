ae364b3cd3f53e657d63213312c962587958ac09
tom <suteaka110@gmail.com>
2018-04-14 20:15:27 +0900

    Added serialization sample.

---
+++ DotNetStandardLib/TestSerialization.cs
@@ -0,0 +1,19 @@
+﻿using Utf8Json;
+
+namespace DotNetStandardLib
+{
+    public static class TestSerialization
+    {
+        public static string Serialize()
+        {
+            var data = new MyData();
+
+            return JsonSerializer.ToJsonString(data);
+        }
+    }
+
+    public class MyData
+    {
+        public string Test { get; set; } = "foo";
+    }
+}
---
+++ UseUtf8jsonInUWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using DotNetStandardLib;
+using Windows.UI.Xaml.Controls;
+
+namespace UseUtf8jsonInUWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Result.Text = TestSerialization.Serialize();
+        }
+    }
+}
